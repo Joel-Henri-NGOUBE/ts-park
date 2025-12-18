961b5bfcf2c0122569656d4bd228d02d2595b6b2
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2025-12-18 13:00:40 +0100

    Middlewares

---
+++ backend/middlewares/authorization.middleware.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken"
+
+export function authMiddleware(req: Request, res: Response, next: NextFunction){
+    const authorization: string | undefined = req.headers.authorization
+    if(!authorization){
+        return res.status(401).end()
+    }
+    const authMembers = authorization?.split(" ") as Array<string>;
+    if(authMembers.length !== 2 || authMembers[0] !== "Bearer") {
+        return res.status(401).end();
+    }
+    const token = authMembers[1]
+    const payload = jwt.verify(token, process.env.JWT_SECRET as string, (error) => {
+        if(error){
+           return res.status(401).json({"message": "Something went wrong with your session token"})
+        }
+    })
+    res.locals.token = payload
+    next()
+}
