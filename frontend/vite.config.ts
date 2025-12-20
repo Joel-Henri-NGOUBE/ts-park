4ef8089c01dbb2a91104176c2aee2e2b167ca0d7
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2025-12-20 10:21:21 +0100

    Frontend Deploy Test -12

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    allowedHosts: ["ts-park-frontend-v1-0-1.onrender.com"]
+  }
 })
